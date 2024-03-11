fd463390254223575fd4d53afd3055eca2c8d560
aryan1309 <aryanjangir1309@gmail.com>
2024-03-11 14:06:19 +0530

    Landing page designed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import CheckModal from "@/components/shared/CheckModal";
 import { ModeToggle } from "@/components/shared/ModeToggle";
+import Header from "@/components/shared/Header";
+import Footer from "@/components/shared/Footer";
+import GenIntro from "@/components/shared/GenIntro";
+import Portfolio1 from "@/components/shared/PortFolio1";
+import PortFolio2 from "@/components/shared/PortFolio2";
+import PortFolio3 from "@/components/shared/PortFolio3";
+import PortFolio4 from "@/components/shared/PortFolio4";
 
 export default function Home() {
 
   return (
+    <div>
+    <Header/>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <ModeToggle />
@@ -12,5 +21,12 @@ export default function Home() {
         <CheckModal />
       </div>
     </main>
+    <GenIntro/>
+    <Portfolio1/>
+    <PortFolio2/>
+    <PortFolio3/>
+    <PortFolio4/>
+    <Footer/>
+    </div>
   );
 }
