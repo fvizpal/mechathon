61d93de7e70ee25827053afce9dc5ccabb96884c
aryan1309 <aryanjangir1309@gmail.com>
2024-03-14 19:58:41 +0530

    Community UI Updated

--- src/components/navigation/NavAddCommunity.tsx
+++ src/components/navigation/NavAddCommunity.tsx
@@ -1,16 +1,24 @@
 "use client"
 
-import { useModal } from "@/hooks/useModalStore"
-import { Button } from "../ui/button"
+import { useModal } from "@/hooks/useModalStore"   
+import { Plus} from "lucide-react";
+import { UserButton } from "../shared/UserButton";
+import { ModeToggle } from "../shared/ModeToggle";
 
 const NavAddCommunity = () => {
   const { onOpen } = useModal();
 
   return (
-    <Button onClick={() => onOpen("createCommunity")}>
-      Add
-    </Button>
-  )
+    <div className="flex gap-7">
+    <UserButton/>
+    <button className="group flex items-center" onClick={() => onOpen("createCommunity")}>
+    <div className="flex mx-3 h-10 w-10 rounded-full group-hover:rounded-lg transition-all overflow-hidden items-center justify-center bg-background dark:bg-netural-700 group-hover:bg-emerald-500">
+    <Plus className="group-hover:text-white transition text-emerald-500" size={35} />
+    </div>
+    </button>
+    <ModeToggle/>
+    </div>
+  ) 
 }
 
 export default NavAddCommunity
--- src/components/navigation/NavigationItem.tsx
+++ src/components/navigation/NavigationItem.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image"
 import { useRouter } from "next/navigation"
+import { ChevronDown } from 'lucide-react';
 
 interface NavigationItemProps {
   id: string,
@@ -17,15 +18,17 @@ const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
 
   return (
     <>
-      <button className="flex bg-emerald-400" onClick={onClick}>
+      <button className="flex bg-emerald-400 w-full rounded-xl hover:bg-orange-200" onClick={onClick}>
         <Image
           src={imageUrl}
           alt="commImage"
-          width={50} height={50}
-          className=" rounded-xl object-contain"
+          width={50} height={25}
+          className="rounded-full object-contain p-1"
         />
-        <span className=" mx-2 w-full h-full font-bold"
-        >{name}</span>
+        <span className="mx-2 w-full h-full font-bold p-3">{name}</span>
+        <button className="flex items-center justify-center m-3 ">
+        <ChevronDown />
+        </button>
       </button>
     </>
   )
