2bb6bc484579c0cf7ce409c83a4a56be56590c8d
aryan1309 <aryanjangir1309@gmail.com>
2024-03-16 18:41:39 +0530

    Setting Modal Created

--- src/components/navigation/NavigationItem.tsx
+++ src/components/navigation/NavigationItem.tsx
@@ -2,7 +2,12 @@
 
 import Image from "next/image"
 import { useRouter } from "next/navigation"
-import { ChevronDown } from 'lucide-react';
+import { SquareUser } from "lucide-react";
+import {
+  Avatar,
+  AvatarImage,
+  AvatarFallback,
+} from "@/components/ui/avatar";
 
 interface NavigationItemProps {
   id: string,
@@ -19,15 +24,14 @@ const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
   return (
     <>
       <button className="flex bg-emerald-400 w-full rounded-xl hover:bg-orange-200" onClick={onClick}>
-        <Image
-          src={imageUrl}
-          alt="commImage"
-          width={50} height={25}
-          className="rounded-full object-contain p-1"
-        />
-        <span className="mx-2 w-full h-full font-bold p-3">{name}</span>
+         <Avatar className="m-1" style={{"height":"50px" , "width":"50px"}}>
+          <AvatarImage src={imageUrl} />
+          <AvatarFallback className="bg-sky-500">
+            <SquareUser className="text-white" />
+          </AvatarFallback>
+        </Avatar>
+        <span className="mx-2 w-full h-full font-bold p-4 flex items-center justify-center">{name}</span>
         <button className="flex items-center justify-center m-3 ">
-          <ChevronDown />
         </button>
       </button>
     </>
