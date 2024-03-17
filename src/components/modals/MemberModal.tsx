03db5d95557190664d136155577b34958ab13c2b
fvizpal <vishalp9966@gmail.com>
2024-03-18 02:32:33 +0530

    fixed type error

--- src/components/modals/MemberModal.tsx
+++ src/components/modals/MemberModal.tsx
@@ -99,7 +99,7 @@ export const MemberModal = () => {
             {community?.members?.map((member) => (
               <div key={member.id} className="flex items-center gap-x-2 mb-6">
 
-                <UserAvatar src={member.user?.image} />
+                <UserAvatar src={member.user?.image as string | undefined} />
                 <div className="flex flex-col gap-y-1">
                   <div className="text-xs font-semibold flex items-center gap-x-1">
                     {member.user?.name}
